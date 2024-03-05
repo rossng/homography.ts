ad1c549473fee868a31da688334f278651a595c5
Ross Gardiner <github@public.rossng.eu>
2024-03-05 22:44:39 +0100

    Add matrix tests

---
+++ packages/core/src/matrix.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from "vitest";
+import { Matrix3x3 } from "./matrix.js";
+
+describe("matrix", () => {
+  test.each([[[1, 2, 1, 3, 4, 1, -1, 2, -1], 8]])(
+    "determinant(%o) -> %d",
+    (matrix, determinant) => {
+      expect(Matrix3x3.determinant(matrix)).toBe(determinant);
+    },
+  ),
+    test.each([
+      [
+        [1, 2, 1, 3, 4, 1, -1, 2, -1],
+        [-3 / 4, 1 / 2, -1 / 4, 1 / 4, 0, 1 / 4, 5 / 4, -1 / 2, -1 / 4],
+      ],
+      [
+        [1, 0, 0, 0, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, -1, -1, 1],
+      ],
+    ])("inverse(%o) -> %o", (matrix, expected) => {
+      const inverse = makeZeroesPositive(Array.from(Matrix3x3.inverse(matrix)));
+      const doubleInverse = makeZeroesPositive(Array.from(Matrix3x3.inverse(inverse)));
+      expect(inverse).toStrictEqual(expected);
+      expect(doubleInverse).toStrictEqual(matrix);
+    });
+});
+
+function makeZeroesPositive(matrix: number[]) {
+  return matrix.map((value) => (value === 0 ? 0 : value));
+}
